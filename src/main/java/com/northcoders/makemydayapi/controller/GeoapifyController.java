871fe0ee207034c6a8b6e7c390bc46592b0175bc
dMarsh <125702046+Alan-Turing13@users.noreply.github.com>
2024-10-02 10:53:25 +0100

    seed data from geoapify restaurants to a local json file

--- src/main/java/com/northcoders/makemydayapi/controller/GeoapifyController.java
+++ src/main/java/com/northcoders/makemydayapi/controller/GeoapifyController.java
@@ -1,16 +1,12 @@
 package com.northcoders.makemydayapi.controller;
 
-import com.northcoders.makemydayapi.model.GeoapifyRestaurant;
 import com.northcoders.makemydayapi.service.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/geoapify/restaurants")
@@ -19,9 +15,10 @@ public class GeoapifyController {
     @Autowired
     RestaurantService restaurantService;
 
-    @GetMapping("{type}")
-    public ResponseEntity<List<GeoapifyRestaurant>> getGeoapifyRestaurants(@PathVariable String type) {
-        return new ResponseEntity<>(restaurantService.getRestaurants(type), HttpStatus.OK);
+    @GetMapping()
+    public ResponseEntity<String> getGeoapifyRestaurants() {
+        restaurantService.getRestaurants();
+        return new ResponseEntity<>("request made", HttpStatus.OK);
     }
 
 }
--- src/main/java/com/northcoders/makemydayapi/service/RestaurantService.java
+++ src/main/java/com/northcoders/makemydayapi/service/RestaurantService.java
@@ -1,20 +1,14 @@
 package com.northcoders.makemydayapi.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.northcoders.makemydayapi.model.GeoapifyRestaurant;
-import com.northcoders.makemydayapi.model.RestaurantType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -29,102 +23,36 @@ public class RestaurantService {
 
     // 10 kilometre radius
     private String radius = "10000";
-    private String resultsLimit = "300";
+    private String resultsLimit = "500";
 
-    @Autowired
-    RestaurantsFilter restaurantsFilter;
 
-    public List<GeoapifyRestaurant> getRestaurants(String type){
-
-        List<GeoapifyRestaurant> restaurants = new ArrayList<>();
+    public String getRestaurants(){
 
         OkHttpClient client = new OkHttpClient.Builder()
                 .readTimeout(0, TimeUnit.SECONDS)
                 .build();
+        System.out.println("client built");
 
         String endpoint = "https://api.geoapify.com/v2/places?categories=catering&filter=circle:"
                 +LONGITUDE+","+LATITUDE+","+radius+"&limit="+resultsLimit+"&apiKey="+API_KEY;
+        System.out.println("endpoint:"+  endpoint);
 
         Request request = new Request.Builder()
                 .url(endpoint)
                 .header("User-Agent", "OkHttp Headers.java")
                 .addHeader("Content-Type", "application/json")
                 .build();
+        System.out.println("built request");
 
-        String jsonBodyString = null;
+        ObjectMapper objectMapper = new ObjectMapper();
 
         try (Response response = client.newCall(request).execute()) {
-            System.out.println("Service 1) Http request executed");
-            jsonBodyString = response.body().string();
+            System.out.println("Got response: ");
+            objectMapper.writeValue(new File("geoapify-restaurants.json"), response.body().string());
+            System.out.println("Wrote json file");
         } catch (IOException e) {
             System.err.println("Error making http request " + e.getMessage());
-            return restaurants;
         }
-
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode jsonBody = null;
-        try {
-            jsonBody = mapper.readTree(jsonBodyString);
-        } catch (Exception e) {
-            System.err.println("Error mapping Json response: " + e.getMessage());
-            return restaurants;
-        }
-
-        if (jsonBody != null) {
-            JsonNode jsonRestaurants = jsonBody.get("features");
-
-            List<JsonNode> filteredRestaurants = restaurantsFilter.process(type, jsonRestaurants);
-            String name, address, imageUrl, phoneNumber, openingHours;
-
-            System.out.println("Service 2) Instantiating the restaurants");
-
-            for (JsonNode restaurant : filteredRestaurants) {
-                address=null;
-                name = restaurant.get("name").asText();
-                JsonNode auxiliaryData = restaurant.get("datasource").get("raw");
-
-                System.out.println("Service 3) For filter " + type + " got " + name);
-
-                // The address may be found at different locations in the JSON
-                try {
-                    address = restaurant.get("formatted").asText()
-                            .replace(", United Kingdom", "")
-                            .replace("London, ", "");
-                    if (address.contains(name)) address = address.replace(name+", ", "");
-                } catch (NullPointerException e) {;}
-                if (address==null){
-                    try {
-                        address = restaurant.get("street").asText()+", "+restaurant.get("postcode").asText();
-                    } catch (NullPointerException e) {;}
-                if (address==null){
-                    try {
-                        address = auxiliaryData.get("addr:street").asText()+", "+auxiliaryData.get("addr:postcode").asText();
-                    } catch (NullPointerException e){;}
-                }
-                }
-
-                try {
-                    imageUrl = auxiliaryData.get("image").asText();
-                } catch (NullPointerException e) {
-                    imageUrl = null;
-                }
-                try {
-                    phoneNumber = auxiliaryData.get("phone").asText();
-                } catch (NullPointerException e) {
-                    phoneNumber = null;
-                }
-                try {
-                    openingHours = auxiliaryData.get("opening_hours").asText();
-                } catch (NullPointerException e) {
-                    openingHours = null;
-                }
-                GeoapifyRestaurant geoapifyRestaurant = new GeoapifyRestaurant(
-                        name, address, imageUrl, phoneNumber, openingHours
-                );
-                System.out.println(geoapifyRestaurant);
-                restaurants.add(geoapifyRestaurant);
-            }
-        }
-        return restaurants;
+        return "restaurant service complete";
     }
 }
